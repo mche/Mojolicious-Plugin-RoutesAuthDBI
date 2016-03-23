10ab6a7373550b8a434d29d4c67192cd1eaf99cd
Mikhail Che <ernesto.chege@yandex.ru>
2016-03-23 15:39:14 +0500

    routes->add_condition

--- lib/Mojolicious/Plugin/RoutesAuthDBI.pm
+++ lib/Mojolicious/Plugin/RoutesAuthDBI.pm
@@ -7,9 +7,19 @@ our $VERSION = '0.02';
 sub register {
   my ($plugin, $app, @args) = @_;
   my $r = $app->routes;
+  $r->add_condition(__PACKAGE__ => \&_auth);
+  # $r->route(...)->over(__PACKAGE__)->... if auth column of sql route record is true
+}
+
+# 
+sub _auth {
+  my ($route, $c, $captures, $patterns) = @_;
+  
 }
 
 1;
+
+
 
 __END__
 
