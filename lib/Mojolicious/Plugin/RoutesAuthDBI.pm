6c37570eb2b89c63d213cf41095adc6de516d8e6
Mikhail Che <ernesto.chege@yandex.ru>
2016-03-23 16:43:05 +0500

    gen routes

--- lib/Mojolicious/Plugin/RoutesAuthDBI.pm
+++ lib/Mojolicious/Plugin/RoutesAuthDBI.pm
@@ -4,14 +4,28 @@ use Mojo::Base 'Mojolicious::Plugin';
 
 our $VERSION = '0.02';
 
+my $dbh;
+
 sub register {
   my ($plugin, $app, $args) = @_;
   $args ||= {};
-  $args->{dbh} ||= $app->dbh;
+  $dbh = $args->{dbh} ||= $app->dbh;
   die "Plugin must work with arg dbh, see SYNOPSIS" unless $args->{dbh};
   my $r = $app->routes;
   $r->add_condition(__PACKAGE__ => \&_auth);
   # $r->route(...)->over(__PACKAGE__=>$route_item)->... 
+  my $sth = $dbh->prepare("select * from routes;");
+  $sth->execute();
+  while (my $r_item = $sth->fetchrow_hashref()) {
+    next if $r_item->{disable};
+    my @request = split /\s+/, $r_item->{request};
+    $r->route(pop @request);
+    $r->via(@request) if @request;
+    $r->over(__PACKAGE__ => $r_item);
+    $r->to(controller=>$r_item->{controller}, action => $r_item->{action},);
+    $r->name($r_item->{name}) if $r_item->{name};
+  }
+  $sth->finish;
 }
 
 # 
