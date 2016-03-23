8c7c84cee8e69a60212c6ee58668a0db324418fe
Mikhail Che <ernesto.chege@yandex.ru>
2016-03-23 14:33:43 +0500

    next commit

--- lib/Mojolicious/Plugin/RoutesAuthDBI.pm
+++ lib/Mojolicious/Plugin/RoutesAuthDBI.pm
@@ -2,11 +2,11 @@ package Mojolicious::Plugin::RoutesAuthDBI;
 use Mojo::Base 'Mojolicious::Plugin';
 
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub register {
   my ($plugin, $app, @args) = @_;
-  
+  my $r = $app->routes;
 }
 
 1;
