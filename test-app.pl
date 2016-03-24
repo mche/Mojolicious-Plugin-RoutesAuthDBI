a1a27e941651c1e10f4f3dc9b2ee5579f1dcfbd6
Mikhail Che <ernesto.chege@yandex.ru>
2016-03-24 18:24:25 +0500

    controll

--- test-app.pl
+++ test-app.pl
@@ -8,10 +8,9 @@ use lib "$FindBin::Bin/lib";
 # This method will run once at server start
 sub startup {# 
   my $app = shift;
-  #~ $app->log->debug($app->dumper($app->SUPER::startup(1,2,3)));
-  $app->plugin(Config =>{file => 'Config.pm'});# нельзя в new
-  has dbh=>sub{ {}; };
-  has sth=>sub{ {}; };
+  $app->plugin(Config =>{file => 'Config.pm'});
+  has dbh => sub { {}; };
+  has sth => sub { {}; };
   $app->plugin('ConfigApply');
   $app->plugin('RoutesAuthDBI', dbh=>$app->dbh->{'main'});
   $app->routes->namespaces(['Controll']);
