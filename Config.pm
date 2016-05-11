b4e2b15ebfa032199c880437d3e7f2d38983067e
Mikhail Che <ernesto.chege@yandex.ru>
2016-05-11 16:42:47 +0500

    che

--- Config.pm
+++ Config.pm
@@ -1,74 +1,104 @@
 
 =pod
-Главный Конфиг сервиса, различные опции для главного модуля
-В него сливаются все отдельные конфиги
-Его имя жестко зашито в app
-Обрабатывается плугином 'ConfigApply'
+Главный Конфиг сервиса
 =cut
 
-#use strict;# Plugin::Config добавляет use Mojo::Base -strict
-#~ use utf8;
 
 {
-	'Проект'=>'Тест плугинов',
-	# установка лог файла раньше установки режима, поэтому всегда log/development.log!!!!
-	#mojo_mode=>$ENV{PLACK_ENV} ? 'production' : 'development', #  production $ENV{ MOJO_MODE}
-	mojo_mode=> 'development',
-	# mode принудительно production если увидит $ENV{PLACK_ENV}
-	mojo_log_level => 'debug',#$ENV{PLACK_ENV} ? 'error' : 'debug', 
-	mojo_plugins=>[ # map $self->plugin($_)
-			#~ ['PODRenderer'], # Documentation browser under "/perldoc"
-			[charset => { charset => 'UTF-8' }, ],
-			['EDumper', helper =>'dumper'],
-			#[PoweredBy => {name => "Perl $^V Web Service"}],
-			#~ [ConfigRoutes => {file=>"ConfigRoutes.pm",},],#"$FindBin::Bin/ConfigRoutes.pm",
-			 #~ ['ConfigApply'],
-			#~ ['HeaderCondition'],
-			#~ ['ParamsArray'],
-	],
-	# Хуки
-	#~ mojo_hooks=>{
-		#~ before_dispatch => sub {1;},
-	#~ },
-	# Хазы (не катят в Plugin::ApplyConfig)
-	#~ mojo_has => {
-		#~ foo=>sub {my $app = shift; },
-	#~ },
-	mojo_secrets => ['true 123 test-app',],
-	#!!! пустой has dbh=>sub{{};}; в app !!!
-	dbh=>{# dsn, user, passwd
-		'main'=>{
-			connect=>["DBI:Pg:dbname=test;", "guest", undef, {# DBI->connect
-				ShowErrorStatement => 1,
-				AutoCommit => 1,
-				RaiseError => 1,
-				PrintError => 1, 
-				pg_enable_utf8 => 1,
-				#mysql_enable_utf8 => 1,
-				#mysql_auto_reconnect=>1,
-				#~ AutoInactiveDestroy => 1,
-			}],
-			do=>['set  datestyle to "ISO, DMY";',],
-			sth=>{# prepared sth
-					'table.columns'=><<SQL,
+  'Проект'=>'Тест плугинов',
+  # установка лог файла раньше установки режима, поэтому всегда log/development.log!!!!
+  #mojo_mode=>$ENV{PLACK_ENV} ? 'production' : 'development', #  production $ENV{ MOJO_MODE}
+  mojo_mode=> 'development',
+  # mode принудительно production если увидит $ENV{PLACK_ENV}
+  mojo_log_level => 'debug',#$ENV{PLACK_ENV} ? 'error' : 'debug', 
+  mojo_plugins=>[ # map $self->plugin($_)
+      #~ ['PODRenderer'], # Documentation browser under "/perldoc"
+      [charset => { charset => 'UTF-8' }, ],
+      ['EDumper', helper =>'dumper'],
+      ['RoutesAuthDBI',
+          dbh=>sub{ shift->dbh->{'main'}},
+          auth=>{current_user_fn=>'auth_user'},
+          #access=> {},
+          admin=>{prefix=>'myadmin', trust=>'fooobaaar'},
+      ],
+      #~ ['HeaderCondition'],
+      #~ ['ParamsArray'],
+  ],
+  mojo_session => {cookie_name => 'SESS'},
+  # Хуки
+  #~ mojo_hooks=>{
+    #~ before_dispatch => sub {1;},
+  #~ },
+  # Хазы (не катят в Plugin::ApplyConfig)
+  #~ mojo_has => {
+    #~ foo=>sub {my $app = shift; },
+  #~ },
+  mojo_secrets => ['true 123 test-app',],
+  #!!! пустой has dbh=>sub{{};}; в app !!!
+  dbh=>{# dsn, user, passwd
+    'main'=>{
+      connect=>["DBI:Pg:dbname=test;", "guest", undef, {# DBI->connect
+        ShowErrorStatement => 1,
+        AutoCommit => 1,
+        RaiseError => 1,
+        PrintError => 1, 
+        pg_enable_utf8 => 1,
+        #mysql_enable_utf8 => 1,
+        #mysql_auto_reconnect=>1,
+        #~ AutoInactiveDestroy => 1,
+      }],
+      do=>['set  datestyle to "ISO, DMY";',],
+      sth=>{# prepared sth
+          'table.columns'=><<SQL,
 select * 
 from information_schema.columns
 where
-	table_catalog='test'
-	and table_schema=? --'public'
-	and table_name=? --'routes'
+  table_catalog='test'
+  and table_schema=? --'public'
+  and table_name=? --'routes'
 ;
 
 
 SQL
-				
-				},
-			
-		}
-	},
-	#!!! пустой has sth=>sub{{};}; в app !!!
-	sth => { main=>{},},# prepared sth
+        
+        },
+      
+    }
+  },
+  #!!! пустой has sth=>sub{{};}; в app !!!
+  sth => { main=>{},},# prepared sth
 #~ now=>"select now();"
+  routes => [
+  [
+    route=>'/callback',
+    over=>{access=>{auth=>1, role=>'admin'}},
+    to=>{cb => sub {shift->render(format=>'txt', text=>'You have access!')}},
+    name=>'foo',#'install#manual', namespace000=>'Mojolicious::Plugin::RoutesAuthDBI',
+  ],[
+    route=>'/check-auth',
+    over=>{access=>sub {my ($user, $route, $c, $captures, $args) = @_; return $user;}},
+    to=>{cb=>sub {my $c =shift; $c->render(format=>'txt', text=>"Hi @{[$c->auth_user->{login}]}! You have access!");}},
+  ],[
+    route=>'/routes',
+    to=>{cb=>sub {my $c =shift; $c->render(format=>'txt', text=>$c->dumper($c->match->endpoint));}},
+    name=>'app routes',
+  ],[
+    route=>'/man',
+    over=>{access=>{auth=>0,}},
+    to=>['install#manual', namespace=>'Mojolicious::Plugin::RoutesAuthDBI',],#
+  ],[
+    route=>'/schema/:schema',
+    over=>{access=>{auth=>0,}},
+    to=>['install#schema', namespace=>'Mojolicious::Plugin::RoutesAuthDBI',],#
+  ],[
+    route=>'/test1',
+    over=>{access=>{auth=>1,}},
+    to=>'test#test1',
+  ],[
+    route=>'/test5/:action',
+    to=>{controller=>'Test'},
+  ],
+  ],
 };
 
 
--- test-app.pl
+++ test-app.pl
@@ -1,43 +1,17 @@
 #!/usr/bin/env perl
-
 # find ~/perl5/lib/ -type f -exec grep -Hni 'is not a controller' {} \;
 
-
 package TestApp;
-use Mojo::Base 'Mojolicious';
+use Mojo::Base 'Mojolicious::Che';
 
 
 use FindBin;
 use lib "$FindBin::Bin/lib";
 
 
-has dbh => sub { {}; };
-has sth => sub { {}; };
-
-# This method will run once at server start
 sub startup {# 
   my $app = shift;
   $app->plugin(Config =>{file => 'Config.pm'});
-  $app->plugin('ConfigApply');
-  $app->plugin('RoutesAuthDBI',
-    dbh=>sub{ shift->dbh->{'main'}},
-    auth=>{current_user_fn=>'auth_user'},
-    access=> {},
-    admin=>{prefix=>'myadmin', trust=>'fooobaaar'},
-  );
-  my $r = $app->routes;
-  #~ push @{ $r->namespaces() }, 'Mojolicious::Plugin::RoutesAuthDBI::Test',;
-  
-  $r->route('/callback')->over(access=>{auth=>1, role=>'admin'})->to(cb => sub {shift->render(format=>'txt', text=>'You have access!')})->name('foo');#'install#manual', namespace000=>'Mojolicious::Plugin::RoutesAuthDBI',
-  $r->route('/check-auth')->over(access=>sub {my ($user, $route, $c, $captures, $args) = @_; return $user;})->to(cb=>sub {my $c =shift; $c->render(format=>'txt', text=>"Hi @{[$c->auth_user->{login}]}! You have access!");});
-  $r->route('/routes')->to(cb=>sub {my $c =shift; $c->render(format=>'txt', text=>$c->dumper($c->match->endpoint));});
-  $r->route('/man')->over(access=>{auth=>0,})->to('install#manual', namespace=>'Mojolicious::Plugin::RoutesAuthDBI',);#
-  $r->route('/schema/:schema')->over(access=>{auth=>0,})->to('install#schema', namespace=>'Mojolicious::Plugin::RoutesAuthDBI',);#
-  $r->route('/test1')->over(access=>{auth=>1,})->to('test#test1', );
-  $r->route('/test5/:action')->to(controller=>'Test');
-  
+  $app->che_go();
 }
-
-
-
 __PACKAGE__->new()->start();
