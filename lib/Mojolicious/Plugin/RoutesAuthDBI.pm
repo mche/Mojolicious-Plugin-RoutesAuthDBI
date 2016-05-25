3d6c0fa2f5dc869d4dbb53042f69cba160d4e9a0
Mikhail Che <ernesto.chege@yandex.ru>
2016-05-25 18:48:22 +0500

    has 0.501

--- lib/Mojolicious/Plugin/RoutesAuthDBI.pm
+++ lib/Mojolicious/Plugin/RoutesAuthDBI.pm
@@ -4,7 +4,7 @@ use Mojo::Loader qw(load_class);
 use Mojo::Util qw(hmac_sha1_sum);
 use Hash::Merge qw( merge );
 
-our $VERSION = '0.500';
+our $VERSION = '0.501';
 
 my $pkg = __PACKAGE__;
 
@@ -297,7 +297,7 @@ sub deny_log {
 
 =head1 VERSION
 
-0.448
+0.501
 
 =head1 NAME
 
