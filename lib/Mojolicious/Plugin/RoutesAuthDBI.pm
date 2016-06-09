ff704f65ed31834763f45d43b797d5241bf207e8
Mikhail Che <ernesto.chege@yandex.ru>
2016-06-09 11:18:21 +0500

    oauth

--- lib/Mojolicious/Plugin/RoutesAuthDBI.pm
+++ lib/Mojolicious/Plugin/RoutesAuthDBI.pm
@@ -4,13 +4,13 @@ use Mojo::Loader qw(load_class);
 use Mojo::Util qw(hmac_sha1_sum);
 use Hash::Merge qw( merge );
 
-our $VERSION = '0.607';
+our $VERSION = '0.608';
 
 =pod
 
 =head1 VERSION
 
-0.607
+0.608
 
 =cut
 
